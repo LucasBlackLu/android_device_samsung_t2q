f99940da1ac1503327e4626526e22c28171122c3
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-04-01 16:50:24 +0200

    Initial a52sxq tree
    Change-Id: I20c6a3347feaf7ddb89e733320d796e83e275114

---
+++ device.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/a52sxq
+
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+
+# call the common setup
+$(call inherit-product, device/samsung/sm7325-common/common.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/a52sxq/a52sxq-vendor.mk)
+
+# Init files
+PRODUCT_PACKAGES += \
+    init.a52sxq.rc
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
