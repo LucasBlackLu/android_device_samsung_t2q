58d1a42a55e51a7e8967db46d30580be0b2cab0f
LucasBlackLu <lucasblack997@gmail.com>
2024-03-09 19:05:21 +0800

    Revert "t2q: Import modules.load"
    This reverts commit bda80a5237eb8c63c381952c7b0aacd7b1983abc.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,10 +15,5 @@ TARGET_SCREEN_DENSITY := 450
 # Kernel config
 TARGET_KERNEL_CONFIG        := vendor/t2q_chn_hkx_defconfig
 
-# Kernel modules
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
-BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
-RECOVERY_KERNEL_MODULES := $(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD)
-
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := t2q
