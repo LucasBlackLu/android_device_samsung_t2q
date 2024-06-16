d2180732113725531c7ef934e463e47b6bcacb4c
LucasBlackLu <lucasblack997@gmail.com>
2024-06-16 17:35:53 +0800

    t2q: Derpfest Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_t2q.mk
+    $(LOCAL_DIR)/derp_t2q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_t2q-userdebug \
-    lineage_t2q-user \
-    lineage_t2q-eng
+    derp_t2q-userdebug \
+    derp_t2q-user \
+    derp_t2q-eng
