679280abb1b7e199e38aaa1193fb634ea4ab6cac
LucasBlackLu <lucasblack997@gmail.com>
2024-06-20 01:53:51 +0800

    t2q: Drop Audio and sensor

--- device.mk
+++ device.mk
@@ -28,14 +28,3 @@ $(call inherit-product, vendor/samsung/t2q/t2q-vendor.mk)
 PRODUCT_PACKAGES += \
     init.t2q.rc
 
-# Audio
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(DEVICE_PATH)/configs/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml \
-
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
-PRODUCT_PACKAGES += \
-    sensors.t2q
