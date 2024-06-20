10d7012d039966359abca0eabc2d8d9fad25f149
LucasBlackLu <lucasblack997@gmail.com>
2024-06-20 19:56:26 +0800

    t2q: Intitial t2q tree

---
+++ BoardConfig.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/t2q
+
+include device/samsung/sm8350-common/BoardConfigCommon.mk
+
+
+# Board
+TARGET_BOARD_NAME := SRPTI01B007
+
+# Display
+TARGET_SCREEN_DENSITY := 450
+
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+TARGET_KERNEL_CONFIG        := t2q_defconfig
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := t2q
+
+# Security patch
+VENDOR_SECURITY_PATCH := 2024-03-01
