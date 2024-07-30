f1d945fe4b8c193832c66ba8dec6981bf421d24f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-30 12:13:59 +0200

    t2q: Setup `TARGET_HAS_UDFPS` flag
    Change-Id: I86bebcac2fa73d12aa48f30cb90d969f6cc3aae4

--- device.mk
+++ device.mk
@@ -42,6 +42,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
