b0471ac8ff3c1802f6f6b5f55261e5468036f35e
LucasBlackLu <lucasblack997@gmail.com>
2025-05-28 09:28:12 +0800

    t2q: Update to user/release-keys

--- lineage_t2q.mk
+++ lineage_t2q.mk
@@ -17,7 +17,10 @@ PRODUCT_MODEL := SM-G9960
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 TARGET_VENDOR := samsung
 TARGET_VENDOR_PRODUCT_NAME := t2q
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="t2qzhx-user 14 UP1A.231005.007 G9960ZHU7GXDE release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="t2qzhx-user 14 UP1A.231005.007 G9960ZHSBGXJ4 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := samsung/t2qzhx/t2q:11/RP1A.200720.012/G9960ZHU7GXDE:user/release-keys
+BUILD_FINGERPRINT := samsung/t2qzhx/t2q:11/RP1A.200720.012/G9960ZHSBGXJ4:user/release-keys
+
+# Use the latest approved GMS identifiers
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
