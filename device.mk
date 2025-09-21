0fc7d79bffe61e0881b6d82bb43c803afdb25f62
Martijn Coenen <maco@google.com>
2025-09-22 01:28:16 +0200

    a23xq: Add android.hardware.nfc.any to NFC features
    * So we can start the NFC process whenever one of
    these is present.
    
    Bug: 36873979
    Bug: 37269025
    Test: NFC is still started on devices with this feature
    Change-Id: I879c084c1ed56e33a4249172c8fa3c04b1c5f236

--- device.mk
+++ device.mk
@@ -30,7 +30,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
