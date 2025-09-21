1aae8d2c758e43f158cefcd1ee852078d219b54e
Jack Yu <jackcwyu@google.com>
2025-09-22 01:27:56 +0200

    t2q: Add Nfc Offhost SecureElements feature definition
    Add FEATURE_NFC_OFF_HOST_CARD_EMULATION_UICC and
    FEATURE_NFC_OFF_HOST_CARD_EMULATION_ESE feature definition.
    
    Bug:129460593
    Test: manual
    Change-Id: I9e79c9ffe9ff30cbb5ba2f0559374854eaa5c336

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_PACKAGES += \
     libchrome.vendor \
     Tag
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+
 # Overlays 
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
