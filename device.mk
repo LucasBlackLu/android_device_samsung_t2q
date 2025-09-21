4528c407697655e3b212c4673619787031df0cfe
Haky86 <hakamelassouad@gmail.com>
2025-09-22 01:27:29 +0200

    t2q: Build basic NFC packages from source
    Change-Id: I0818efaa3ac8840816bb0983fc0daead2e2e067a

--- device.mk
+++ device.mk
@@ -18,6 +18,13 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, device/samsung/sm8350-common/common.mk)
 
 # NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2.vendor \
+    com.android.nfc_extras \
+    libchrome \
+    libchrome.vendor \
+    Tag
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
