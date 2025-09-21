f7db7d7d923cfaf9e5b719fe4ae0a53f919bb27d
Martijn Coenen <maco@google.com>
2025-09-22 01:29:00 +0200

    a23xq: Copy com.nxp.mifare from frameworks/native
    Change-Id: I4dbec41cbf062334af8d66780859736f0ee5dab3

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
