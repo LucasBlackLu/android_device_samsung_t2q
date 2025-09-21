707e5a2078108947f9efa227f76d5be3d13ae294
Haky86 <hakamelassouad@gmail.com>
2025-09-22 00:27:25 +0200

    t2q: Import init.t2q.rc
    Change-Id: I53fd28a36eca3fa98a9bddba37835d007b73c5d5

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Init
+PRODUCT_PACKAGES += \
+    init.t2q.rc
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
