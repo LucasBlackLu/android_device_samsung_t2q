eddb8367a3e94fd6780c0f333740e5113096b9b7
Ramanan Rajeswaran <ramanan@google.com>
2025-09-22 01:28:34 +0200

    a23xq: Add com.android.nfc_extras to native/for pdk
    Change-Id: Ife40ac33241878814f5d9e027e43cd176c57fb56

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
