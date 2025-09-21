25c71340b0cb3df1b289e738c24eacebdafa929d
LucasBlackLu <lucasblack997@gmail.com>
2025-09-22 00:00:02 +0200

    t2q: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+-include device/samsung/sm8350-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/t2q
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := t2q
+
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Display
+TARGET_SCREEN_DENSITY := 450
+
+# Kernel
+TARGET_KERNEL_CONFIG := t2q_defconfig
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Security Patch Level
+VENDOR_SECURITY_PATCH := 2025-04-01
+
+# Get non-open-source specific aspects
+include vendor/samsung/t2q/BoardConfigVendor.mk
